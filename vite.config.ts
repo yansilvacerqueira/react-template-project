f958c8127622d8b8e509754cc49bdeaec4fc580b
Yan Cerqueira <yansilvacerqueira@outlook.com>
2024-12-09 14:23:54 -0300

    feat: adding alias path config on vite and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src", "components")
+    }
   }
 })
