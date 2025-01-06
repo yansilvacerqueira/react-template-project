b9f956c09a43b1b1b179ff6a62b8cf0539b1ab38
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-05 23:40:36 -0300

    feat: fixing dynamic error imports on vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "@/app/contexts/ThemeProvider"
 import { Router } from "@/app/router/Router"
 
+import '@/lib/dyamicImportErrorListener'
+
 export const App = () => {
   return (
     <ThemeProvider>
